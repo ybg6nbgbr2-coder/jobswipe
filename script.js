a077eb1b90eed2d5af2a7acd5da6b8479849f28a
ybg6nbgbr2-coder <ybg6nbgbr2@privaterelay.appleid.com>
2026-07-18 05:37:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ import {
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAfvX_iWxK5dcbqFM5onp6-WQgjH1ysb1o",
   authDomain: "jobswipe-d9308.firebaseapp.com",
